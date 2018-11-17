9109265cb51160fec7d4689338cc58b1f04532a5
yua-46 <yuayua4646@yahoo.co.jp>
2018-11-18 04:01:39 +0900

    l

--- okonomiyaki-miyuki/aiueo.js
+++ okonomiyaki-miyuki/aiueo.js
@@ -75,59 +75,22 @@
 
 scrolltotop.init()*/
 
-// グローバル変数
-var syncerTimeout = null ;
+// 最上部から現在位置までの距離を取得して、変数[now]に格納
+var now = $( window ).scrollTop() ;
 
-// 一連の処理
-$( function()
+// 最上部から現在位置までの距離(now)が1500px以上だったら
+if( now > 500 )
 {
-	// スクロールイベントの設定
-	$( window ).scroll( function()
-	{
-		// 1秒ごとに処理
-		if( syncerTimeout == null )
-		{
-			// セットタイムアウトを設定
-			syncerTimeout = setTimeout( function(){
+	// [#page-top]をゆっくりフェードインする
+	$( '#page-top' ).fadeIn( "slow" ) ;
+}
 
-				// 対象のエレメント
-				var element = $( '#page-top' ) ;
-
-				// 現在、表示されているか？
-				var visible = element.is( ':visible' ) ;
-
-				// 最上部から現在位置までの距離を取得して、変数[now]に格納
-				var now = $( window ).scrollTop() ;
-
-				// 最下部から現在位置までの距離を計算して、変数[under]に格納
-				var under = $( 'body' ).height() - ( now + $(window).height() ) ;
-
-				// 最上部から現在位置までの距離(now)が1500以上かつ
-				// 最下部から現在位置までの距離(under)が200px以上かつ…
-				if( now > 1500 && 200 < under )
-				{
-					// 非表示状態だったら
-					if( !visible )
-					{
-						// [#page-top]をゆっくりフェードインする
-						element.fadeIn( 'slow' ) ;
-					}
-				}
-
-				// 1500px以下かつ
-				// 表示状態だったら
-				else if( visible )
-				{
-					// [#page-top]をゆっくりフェードアウトする
-					element.fadeOut( 'slow' ) ;
-				}
-
-				// フラグを削除
-				syncerTimeout = null ;
-			} , 1000 ) ;
-		}
-	} ) ;
-
+// 1500px以下だったら
+else
+{
+	// [#page-top]をゆっくりフェードアウトする
+	$( '#page-top' ).fadeOut( 'slow' ) ;
+}
 	// クリックイベントを設定する
 	$( '#move-page-top' ).click(
 		function()
@@ -135,5 +98,3 @@ $( function()
 			// スムーズにスクロールする
 			$( 'html,body' ).animate( {scrollTop:0} , 'slow' ) ;
 		}
-	) ;
-} ) ;
