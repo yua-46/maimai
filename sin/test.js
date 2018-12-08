96784615e1366f8d830dee911acf5f2c7f7c0b20
yua-46 <yuayua4646@yahoo.co.jp>
2018-12-08 23:52:06 +0900

    j

--- sin/test.js
+++ sin/test.js
@@ -1,10 +1,16 @@
+var $section = $('.js-section'); // 各スライド
+var $pager = $('#js-pager'); // ページャー枠
+
+// scrollifyのオプション設定
 var option = {
-    section : '.js-section',
-    easing: "swing",
-    scrollSpeed: 600,
-    scrollbars: true,
-  };
-  
-  $(function() {
-    $.scrollify(option); // scrollifyの実行
-  });
+  section : '.js-section',
+  easing: "swing",
+  scrollSpeed: 600,
+  scrollbars: true,
+  before:function(index) {
+    pagerCurrent(index); // ページャーに対応する順番にクラス名を付与
+  },
+  afterRender:function() {
+    createPager(); // ページャーの作成
+  }
+};
