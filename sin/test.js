f82b69c8c37ff91bf439c259f3496ebc86f746ac
yua-46 <yuayua4646@yahoo.co.jp>
2018-12-10 10:12:39 +0900

    j

--- sin/test.js
+++ sin/test.js
@@ -1,3 +1,34 @@
-$(function(){
+$(function() {
   
-});
+    function toggleChangeBtn() {
+      var slideIndex = $('.slide').index($('.active'));
+      $('.change-btn').show();
+      if (slideIndex == 0) {
+        $('.prev-btn').hide();
+      // 「3」の部分を、lengthメソッドを用いて書き換えてください
+      } else if (slideIndex == $('.slide').length - 1) {
+        $('.next-btn').hide();
+      }
+    }
+    
+    $('.index-btn').click(function() {
+      $('.active').removeClass('active');
+      var clickedIndex = $('.index-btn').index($(this));
+      $('.slide').eq(clickedIndex).addClass('active');
+      toggleChangeBtn();
+      
+    });
+    
+    $('.change-btn').click(function() {
+      var $displaySlide = $('.active');
+      $displaySlide.removeClass('active');
+      if ($(this).hasClass('next-btn')) {
+        $displaySlide.next().addClass('active');
+      } else {
+        $displaySlide.prev().addClass('active');
+      }
+      toggleChangeBtn();
+      
+    });
+  });
+  
