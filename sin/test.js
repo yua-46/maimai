577c6666923b44f51e65368759c687eedf452570
yua-46 <yuayua4646@yahoo.co.jp>
2018-12-10 18:00:00 +0900

    p

--- sin/test.js
+++ sin/test.js
@@ -1,34 +1,20 @@
+function slideSwitch() {
+   var $active = $('#slideshow img.active');
+
+   if ( $active.length == 0 ) $active = $('#slideshow img:last');
+
+   var $next =  $active.next().length ? $active.next()
+      : $('#slideshow img:first');
+
+   $active.addClass('last-active');
+
+   $next.css({opacity: 0.0})
+      .addClass('active')
+      .animate({opacity: 1.0}, 1000, function() {
+           $active.removeClass('active last-active');
+      });
+}
+
 $(function() {
-  
-    function toggleChangeBtn() {
-      var slideIndex = $('.slide').index($('.active'));
-      $('.change-btn').show();
-      if (slideIndex == 0) {
-        $('.prev-btn').hide();
-      // 「3」の部分を、lengthメソッドを用いて書き換えてください
-      } else if (slideIndex == $('.slide').length - 1) {
-        $('.next-btn').hide();
-      }
-    }
-    
-    $('.index-btn').click(function() {
-      $('.active').removeClass('active');
-      var clickedIndex = $('.index-btn').index($(this));
-      $('.slide').eq(clickedIndex).addClass('active');
-      toggleChangeBtn();
-      
-    });
-    
-    $('.change-btn').click(function() {
-      var $displaySlide = $('.active');
-      $displaySlide.removeClass('active');
-      if ($(this).hasClass('next-btn')) {
-        $displaySlide.next().addClass('active');
-      } else {
-        $displaySlide.prev().addClass('active');
-      }
-      toggleChangeBtn();
-      
-    });
-  });
-  
+   setInterval( "slideSwitch()", 4000 );
+});
