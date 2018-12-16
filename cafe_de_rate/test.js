2384a5a129b2c5a9171668bdc10899b2df1a0b14
yua-46 <yuayua4646@yahoo.co.jp>
2018-12-17 03:22:21 +0900

    k

--- cafe_de_rate/test.js
+++ cafe_de_rate/test.js
@@ -35,3 +35,44 @@ $(function() {
  });
 });
 
+$(function () {
+
+  // falg変数のデフォルト値として「up」を指定    
+  var flag = "up";
+
+  // scrollイベントを取得した際の処理を定義
+  $(window).on("scroll", function () {
+    // scrollTop()が「0」より大きい場合
+    if ($(this).scrollTop() > 200) {
+　　　　// flag変数が「up」だった場合の処理
+      if (flag === "up") {
+        // ヘッダーバーに対して、stop()メソッドを実行してから、
+        // animate()メソッドを実行
+        $(".cb-header").stop().animate({
+          // topの位置を「-56px」から「0」になるまでアニメーション
+          top: 0
+        // アニメーション時間を「500ms」に設定
+        }, 500)
+        // flag変数の値を「down」に変更
+        flag = "down";
+      }
+    // scrollTop()が「0」の場合
+    } else {
+      // flag変数が「down」だった場合の処理
+      if (flag === "down") {
+        // ヘッダーバーに対して、stop()メソッドを実行してから、
+        // animate()メソッドを実行
+        $(".cb-header").stop().animate({
+          // topの位置を「0」から「-56px」になるまでアニメーション
+          top: "-200px"
+        // アニメーション時間を「500ms」に設定
+        }, 500);
+        // flag変数の値を「up」に変更
+        flag = "up";
+      }
+
+    }
+
+  });
+
+});
